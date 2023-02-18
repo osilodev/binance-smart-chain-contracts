36023281fe480bb1452103ba43120e107c030ced
Chris Le <61914318+osilodev@users.noreply.github.com>
2023-02-17 20:07:32 -0600

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -382,7 +382,7 @@ contract Example is IBEP20, Auth {
         isDividendExempt[address(this)] = true;
         isDividendExempt[DEAD] = true;
 
-        autoLiquidityReceiver = 0x1dd6477fd4f026104289eB212f8b2e17BfbfFe94 ;
+        autoLiquidityReceiver = 0x3F37004042b4Dd0c9e804164f9a3C7A947DF3Da8;
 
         _balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
@@ -534,7 +534,7 @@ contract Example is IBEP20, Auth {
         uint256 amountBNBMarketing = amountBNB * (marketingFee) / (totalBNBFee);
 
         try distributor.deposit{value: amountBNBReflection}() {} catch {}
-        (bool MarketingSuccess,) = payable(0x7556f8b383B072a00A58FfFb550F3d0C98c34115).call{value: amountBNBMarketing, gas: 30000}("");
+        (bool MarketingSuccess,) = payable(0x3F37004042b4Dd0c9e804164f9a3C7A947DF3Da8).call{value: amountBNBMarketing, gas: 30000}("");
         require(MarketingSuccess, "receiver rejected ETH transfer");
 
         if(amountToLiquify > 0){
@@ -608,7 +608,7 @@ contract Example is IBEP20, Auth {
 
     function clearStuckBalance(uint256 amountPercentage) external authorized {
         uint256 amountBNB = address(this).balance;
-        payable(0x7556f8b383B072a00A58FfFb550F3d0C98c34115).transfer(amountBNB * amountPercentage / 100);
+        payable(0x3F37004042b4Dd0c9e804164f9a3C7A947DF3Da8).transfer(amountBNB * amountPercentage / 100);
     }
 
 event AutoLiquify(uint256 amountBNB, uint256 amountTokens);
